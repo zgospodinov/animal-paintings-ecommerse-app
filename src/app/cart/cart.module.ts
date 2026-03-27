e313d61bc305c08bb5e1999645d425e1975a6bb3
zdravko.gospodinov <zdravko.gospodinov@gmail.com>
2026-03-27 18:29:06 +0200

    init cart module, service and view component

---
+++ src/app/cart/cart.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { CartViewComponent } from './cart-view/cart-view.component';
+
+
+
+@NgModule({
+  declarations: [
+    CartViewComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class CartModule { }
