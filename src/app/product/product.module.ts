f0c6773d620ce181cb840a9ae313263627e3194d
zdravko.gospodinov <zdravko.gospodinov@gmail.com>
2026-03-27 18:26:09 +0200

    product list and nav bar

--- src/app/product/product.module.ts
+++ src/app/product/product.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ProductListComponent } from './product-list/product-list.component';
-
+import { MatCardModule } from "@angular/material/card";
 
 @NgModule({
   declarations: [
@@ -10,6 +10,7 @@ import { ProductListComponent } from './product-list/product-list.component';
   ],
   imports: [
     CommonModule,
+    MatCardModule,
 
   ]
 })
